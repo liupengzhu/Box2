e7b6a92edfced860528f16b3f58ee5e4e26679cd
朱留鹏 <>
2018-01-19 15:33:34 +0800

    个人设置界面布局完成

--- app/src/main/java/com/example/box/PersonalSettingActivity.java
+++ app/src/main/java/com/example/box/PersonalSettingActivity.java
@@ -1,6 +1,5 @@
 package com.example.box;
 
-import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
@@ -8,22 +7,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.RelativeLayout;
 
 import com.example.titlebar.TitleBar;
 import com.example.titlebar.TitleListener;
 
-public class PersonalInfoActivity extends AppCompatActivity implements View.OnClickListener {
+public class PersonalSettingActivity extends AppCompatActivity {
 
     private TitleBar titleBar;
-
-    RelativeLayout settingButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_personal_info);
-
+        setContentView(R.layout.activity_personal_setting);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             Window window = getWindow();
             window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
@@ -38,18 +33,14 @@ public class PersonalInfoActivity extends AppCompatActivity implements View.OnCl
      * 初始化view
      */
     private void initView() {
-
-        titleBar = findViewById(R.id.personal_info_title_bar);
-        settingButton = findViewById(R.id.personal_info_setting);
-        settingButton.setOnClickListener(this);
-        titleBar.setTextViewText("个人信息");
-        titleBar.setRightButtonSrc(0);
+        titleBar = findViewById(R.id.personal_setting_title_bar);
+        titleBar.setTextViewText("设置");
         titleBar.setLeftButtonVisible(View.GONE);
+        titleBar.setRightButtonSrc(0);
         titleBar.setLeftBackButtonVisible(View.VISIBLE);
         titleBar.setOnClickListener(new TitleListener() {
             @Override
             public void onLeftButtonClickListener(View v) {
-
             }
 
             @Override
@@ -62,26 +53,5 @@ public class PersonalInfoActivity extends AppCompatActivity implements View.OnCl
 
             }
         });
-
-
-    }
-
-    /**
-     * 点击事件监听
-     *
-     * @param v
-     */
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.personal_info_setting:
-                Intent settingIntent = new Intent(this, PersonalSettingActivity.class);
-                startActivity(settingIntent);
-                break;
-            default:
-                break;
-
-        }
-
     }
 }

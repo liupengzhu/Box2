227bc12f324d7792fb468ceaa0387c0b7971e5cd
朱留鹏 <>
2018-01-19 17:07:27 +0800

    拍照跟换头像和从相册跟换头像功能实现

--- app/src/main/java/com/example/box/PersonalInfoActivity.java
+++ app/src/main/java/com/example/box/PersonalInfoActivity.java
@@ -1,8 +1,21 @@
 package com.example.box;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.ContentUris;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Build;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,12 +27,22 @@ import com.example.selfdialog.PhotoDialog;
 import com.example.titlebar.TitleBar;
 import com.example.titlebar.TitleListener;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class PersonalInfoActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TitleBar titleBar;
 
     RelativeLayout settingButton;
     RelativeLayout photoButton;
+    private Uri imageUri;
+    public static final int TAKE_PHOTO = 1;
+    public static final int CHOOSE_ALBUM = 0;
+    CircleImageView photo;
+    private PhotoDialog photoDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +69,7 @@ public class PersonalInfoActivity extends AppCompatActivity implements View.OnCl
         settingButton.setOnClickListener(this);
         photoButton = findViewById(R.id.personal_info_setting_photo);
         photoButton.setOnClickListener(this);
+        photo = findViewById(R.id.personal_info_photo);
         titleBar.setTextViewText("个人信息");
         titleBar.setRightButtonSrc(0);
         titleBar.setLeftButtonVisible(View.GONE);
@@ -83,7 +107,19 @@ public class PersonalInfoActivity extends AppCompatActivity implements View.OnCl
                 startActivity(settingIntent);
                 break;
             case R.id.personal_info_setting_photo:
-                PhotoDialog photoDialog = new PhotoDialog(this);
+                photoDialog = new PhotoDialog(this);
+                photoDialog.setCameraButtonOnClick(new PhotoDialog.CameraOnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        cameraPhoto();
+                    }
+                });
+                photoDialog.setPhotoButtonOnClick(new PhotoDialog.PhotoOnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        chooseFromAlbum();
+                    }
+                });
                 photoDialog.show();
                 break;
             default:
@@ -92,4 +128,154 @@ public class PersonalInfoActivity extends AppCompatActivity implements View.OnCl
         }
 
     }
+
+    /**
+     * 调用相机拍照
+     */
+    private void cameraPhoto() {
+        File outputImage = new File(getExternalCacheDir(), "output_image.jpg");
+        try {
+            if (outputImage.exists()) {
+                outputImage.delete();
+            }
+            outputImage.createNewFile();
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if (Build.VERSION.SDK_INT >= 24) {
+            imageUri = FileProvider.getUriForFile(PersonalInfoActivity.this,
+                    "com.example.cameraalbumtest.fileprovider", outputImage);
+        } else {
+            imageUri = Uri.fromFile(outputImage);
+        }
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        startActivityForResult(intent, TAKE_PHOTO);
+    }
+
+    /**
+     * 根据返回的请求码设置头像
+     *
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case TAKE_PHOTO:
+                if (resultCode == RESULT_OK) {
+                    try {
+                        Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri));
+                        photo.setImageBitmap(bitmap);
+                        photoDialog.cancel();
+
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+
+
+                }
+                break;
+            case CHOOSE_ALBUM:
+                if (resultCode == RESULT_OK) {
+                    if (Build.VERSION.SDK_INT >= 19) {
+                        handleImageOnKitKat(data);
+                    } else {
+                        handleImageBeforeKitKat(data);
+                    }
+                }
+                break;
+
+        }
+
+    }
+
+    /**
+     * 从相册选取照片
+     */
+    private void chooseFromAlbum() {
+
+        if (ContextCompat.checkSelfPermission(PersonalInfoActivity.this,
+                Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+        } else {
+            openAlbum();
+        }
+    }
+
+    /**
+     * 打开相册
+     */
+    private void openAlbum() {
+        Intent intent = new Intent("android.intent.action.GET_CONTENT");
+        intent.setType("image/*");
+        startActivityForResult(intent, CHOOSE_ALBUM);
+
+    }
+
+    private void handleImageBeforeKitKat(Intent data) {
+        Uri uri = data.getData();
+        String imagePath = getImagePath(uri, null);
+        displayImage(imagePath);
+
+    }
+
+
+    @TargetApi(19)
+    private void handleImageOnKitKat(Intent data) {
+        String imagePath = null;
+        Uri uri = data.getData();
+        if (DocumentsContract.isDocumentUri(this, uri)) {
+            //如果是document类型的uri，则通过documentId处理
+            String docId = DocumentsContract.getDocumentId(uri);
+            if ("com.android.providers.media.documents".equals(uri.getAuthority())) {
+                String id = docId.split(":")[1];
+                String selection = MediaStore.Images.Media._ID + "=" + id;
+                imagePath = getImagePath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, selection);
+            } else if ("com.android.providers.downloads.documents".equals(uri.getAuthority())) {
+                Uri contentUri = ContentUris.withAppendedId(
+                        Uri.parse("content://downloads/public_downloads"), Long.valueOf(docId));
+                imagePath = getImagePath(contentUri, null);
+
+            }
+
+        } else if ("content".equalsIgnoreCase(uri.getScheme())) {
+            //如果是content了类型的Uri，则使用普通方法处理
+            imagePath = getImagePath(uri, null);
+        } else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            //如果是file 类型的uri 直接获取图片路径即可
+            imagePath = uri.getPath();
+        }
+        displayImage(imagePath);
+
+
+    }
+
+
+    private void displayImage(String imagePath) {
+        if (imagePath != null) {
+            photo.setImageBitmap(BitmapFactory.decodeFile(imagePath));
+            photoDialog.cancel();
+        }
+
+    }
+
+
+    private String getImagePath(Uri externalContentUri, String selection) {
+        String path = null;
+        Cursor cursor = getContentResolver().query(externalContentUri, null, selection, null, null);
+        if (cursor != null) {
+            while (cursor.moveToNext()) {
+                path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+            }
+            cursor.close();
+        }
+        return path;
+
+    }
+
+
 }

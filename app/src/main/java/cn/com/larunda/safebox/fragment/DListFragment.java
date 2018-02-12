bac351dd1d6eaf51d8fcad7a1c3165656144dd26
朱留鹏 <>
2018-02-12 14:52:11 +0800

    搜索框软键盘回城键修改为搜索

--- app/src/main/java/cn/com/larunda/safebox/fragment/DListFragment.java
+++ app/src/main/java/cn/com/larunda/safebox/fragment/DListFragment.java
@@ -16,6 +16,7 @@ import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -446,7 +447,7 @@ public class DListFragment extends Fragment implements View.OnClickListener {
                             }
                         });
                     }
-                }else {
+                } else {
                     getActivity().runOnUiThread(new Runnable() {
                         @Override
                         public void run() {

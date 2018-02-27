507441f8659853c20f20cea3d43ae7b3561801cb
朱留鹏 <>
2018-02-27 10:42:26 +0800

    绑定蓝牙界面布局实现C

---
+++ app/src/main/java/cn/com/larunda/safebox/BLEActivity.java
@@ -0,0 +1,13 @@
+package cn.com.larunda.safebox;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BLEActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ble);
+    }
+}

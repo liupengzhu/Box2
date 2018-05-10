2537d0e6505446f6dae658aca4f1d887d9793db5
朱留鹏 <>
2018-05-10 08:38:04 +0800

    UPD 企业管理员界面添加静态界面

--- app/src/main/java/cn/com/larunda/safebox/fragment/AdminHomeFragment.java
+++ app/src/main/java/cn/com/larunda/safebox/fragment/AdminHomeFragment.java
@@ -1,6 +1,21 @@
 package cn.com.larunda.safebox.fragment;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.larunda.safebox.R;
 
 public class AdminHomeFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.home_fragment, container, false);
+        return view;
+    }
 }
